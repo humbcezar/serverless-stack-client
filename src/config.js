eb7e1ff2872ad8c4230e38ceb5d37c98ca028513
Humberto <skateforevis@hotmail.com>
2020-03-08 20:27:21 -0300

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "betoncios-notes-apps-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://6h7xe9zo36.execute-api.us-east-2.amazonaws.com/prod/"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_u1xyrx7lJ",
+    APP_CLIENT_ID: "5d7bp55e7fdljmtdvlrjqni4vq",
+    IDENTITY_POOL_ID: "us-east-2:26adfc4e-2a5c-4fd5-9593-a466a05047d7"
+  }
+};
