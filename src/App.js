d63e48b7552a33cdc07e6a33aeaff1df8c068354
Humberto <skateforevis@hotmail.com>
2020-03-15 23:04:55 -0300

    Setting up our React app 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Routes from "./Routes";
 import { Link } from "react-router-dom";
@@ -6,7 +6,12 @@ import { Nav, Navbar, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
 
+
 function App(props) {
+  function handleLogout() {
+    userHasAuthenticated(false);
+  }
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
   return (
     <div className="App container">
       <Navbar fluid collapseOnSelect>
@@ -18,16 +23,21 @@ function App(props) {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <LinkContainer to="/signup">
-              <NavItem>Signup</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <NavItem>Login</NavItem>
-            </LinkContainer>
+            {isAuthenticated
+              ? <NavItem onClick={handleLogout}>Logout</NavItem>
+              : <>
+                  <LinkContainer to="/signup">
+                    <NavItem>Signup</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <NavItem>Login</NavItem>
+                  </LinkContainer>
+                </>
+            }
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Routes />
+      <Routes appProps={{ isAuthenticated, userHasAuthenticated }} />
     </div>
   );
 }
